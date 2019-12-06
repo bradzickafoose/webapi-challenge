5685ae157fd67cd36e5f817565369fd28fd9edd6
Brad <brad@viziworx.com>
2019-12-06 13:20:34 -0500

    Added /GET method to read action by id. Created middleware to validate by action id

--- actions/action-routes.js
+++ actions/action-routes.js
@@ -19,4 +19,34 @@ router.get('/', (req, res) => {
         });
 });
 
+// GET – READ action by id
+router.get('/:id', validateActionId, (req, res) => {
+    res
+        .status(200)
+        .json(req.action);
+});
+
+// Custom middleware
+function validateActionId(req, res, next) {
+    const actionId = req.params.id;
+
+    Actions
+        .get(actionId)
+        .then(action => {
+            if (action) {
+                req.action = action;
+                next();
+            } else {
+                res
+                    .status(400)
+                    .json({ message: "Invalid action id" });
+            }
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json({ message: "Error retrieving action" })
+        });
+}
+
 module.exports = router;
