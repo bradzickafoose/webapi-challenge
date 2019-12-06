ae071bf55016c448b345dd9c889dc026c19c0e5c
Brad <brad@viziworx.com>
2019-12-06 13:40:33 -0500

    Used   create-react-app <project-directory>
    For example:
      create-react-app my-react-app
    
    Run create-react-app --help to see all options. to create an application to show a list of all projects

---
+++ client/src/components/ProjectList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ProjectList extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>List of Projects</h1>
+            </div>
+        );
+    }
+}
+
+export default ProjectList;
