f68904a051f9e7e051a605712e51363c375627ab
Brad <brad@viziworx.com>
2020-05-18 21:10:39 -0400

    changes default port to 5000

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const server = require('./server.js');
 
-const port = process.env.PORT || 7000;
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
     console.log(`\n*** Server running on http://localhost:${port} ***\n`);
 });
